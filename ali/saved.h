6c909458badcaad335ca1c60de18f51b58eb501c
A18 <alistair.manicardi.carlo@gmail.com>
2023-05-20 17:32:21 +0200

    Finished ali::saved. Added ali::saved::mirror, a namespace for saving both in the current process' ram and in the .ini file.

--- ali/saved.h
+++ ali/saved.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string> // TODO: use char*
+#include <vector>
 #include <map>
 #include <Windows.h>
 #include "convert.h" // TODO: not use this
@@ -34,50 +35,121 @@ namespace ali
 			}
 		}
 
+		namespace mirror {
+			class variable
+			{
+			public:
+				variable(string name, string value = "", string path = iniPath() + "\\settings.ini")
+				{
+					string x = raw::readFile(deafultvariablesection, name);
+					if (value != "") {
+						this->name = name;
+						this->value = value;
+						this->path = path;
+						set(value);
+					}
+					else {
+						this->name = name;
+						this->value = x;
+						this->path = path;
+					}
+				}
+				~variable() {}
+
+				void set(string value)
+				{
+					raw::writeFile(deafultvariablesection, name, value, path);
+					this->value = value;
+				}
+				string get()
+				{
+					return value;
+				}
+				void operator=(const string& val) {
+					raw::writeFile(deafultvariablesection, name, val, path);
+					value = val;
+				}
+				operator std::string const()
+				{
+					return value;
+				}
+			private:
+				string path;
+				string name;
+				string value;
+			};
+			class vector
+			{
+			public:
+				vector(string name) {
+					this->name = name;
+					string x = raw::readFile(name, "size");
+					try {
+						p_size = std::stoi(x);
+					}
+					catch (...) {
+						raw::writeFile(name, "size", "0");
+						p_size = 0;
+					}
+					for (size_t i = 0; i < p_size; i++)
+					{
+						vec.push_back(raw::readFile(name, to_string(i)));
+					}
+				}
+				~vector() {}
+				void push_back(string element)
+				{
+					raw::writeFile(name, "size", to_string(p_size + 1));
+					raw::writeFile(name, to_string(p_size), element);
+					vec.push_back(element);
+					p_size++;
+				}
+				std::string operator [](const size_t& index) {
+					return vec[index];
+				}
+				void operator+=(string element) {
+					push_back(element);
+				}
+				size_t size() { return p_size; }
+			private:
+				string name;
+				size_t p_size;
+				std::vector<string> vec;
+			};
+		}
 
 		class variable
 		{
 		public:
-			variable(string name, string value = "", string path = iniPath()+"\\settings.ini")
+			variable(string name, string value = "", string path = iniPath() + "\\settings.ini")
 			{
 				string x = raw::readFile(deafultvariablesection, name);
-				if (value != "") {
-					this->name = name;
-					this->value = value;
-					this->path = path;
+				if (value != "") 
 					set(value);
-				}
-				else {
-					this->name = name;
-					this->value = x;
-					this->path = path;
-				}
+				this->name = name;
+				this->path = path;
 			}
-			~variable(){}
+			~variable() {}
 
 			void set(string value)
 			{
 				raw::writeFile(deafultvariablesection, name, value, path);
-				this->value = value;
 			}
 			string get()
 			{
-				return value;
+				return raw::readFile(deafultvariablesection, name, path);
 			}
 			void operator=(const string& val) {
 				raw::writeFile(deafultvariablesection, name, val, path);
-				value = val;
 			}
 			operator std::string const()
 			{
-				return value;
+				return raw::readFile(deafultvariablesection, name, path);
 			}
 		private:
 			string path;
 			string name;
-			string value;
 		};
-
 		class vector
 		{
 		public:
@@ -107,8 +179,7 @@ namespace ali
 			string name;
 			size_t p_size;
 		};
-		
-		class map // The value ISN'T saved on the ram, but on the hdd!
+		class map // Currently not supported in ali::saved::mirror
 		{
 		public:
 			map(string name, string path = iniPath() + "\\settings.ini") {
